bef433ea7cf634d881d6a09810363c7bf9c2f07b
ricsjs <ricfilho0007@gmail.com>
2024-10-12 09:55:33 -0300

    feat: project header and footer

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <Navbar />
+  <Navbar :logo="logo_src" :alt="app_name"/>
   <router-view />
   <Footer />
 </template>
@@ -13,7 +13,32 @@ export default {
     Navbar,
     Footer,
   },
+  data() {
+    return {
+      logo_src: "/img/logo.png",
+      app_name: "Make Your Burger"
+    }
+  }
 };
 </script>
 
-<style></style>
+<style>
+  *{
+    margin: 0;
+    padding: 0;
+    font-family: Helvetica;
+    box-sizing: border-box;
+  }
+
+  .main-container {
+    margin: 50px;
+    min-height: 100vh;
+  }
+
+  h1 {
+    text-align: center;
+    font-size: 42px;
+    margin-bottom: 30px;
+    color: #222;
+  }
+</style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template lang="pt-br">
-  <nav>
-    <router-link to="/">
-        <img src="" id="logo">
+  <nav id="nav">
+    <router-link to="/" id="logo_url">
+        <img :src="logo" :alt="alt" id="logo">
     </router-link>
     <router-link to="/">Home</router-link> |
     <router-link to="/pedidos">Pedidos</router-link>
@@ -10,6 +10,37 @@
 <script>
 export default {
   name: "Navbar",
+  props: ["logo", "alt"]
 };
 </script>
-<style></style>
+<style scoped>
+    #nav {
+        background-color: #222;
+        border-bottom: 4px solid #111;
+        padding: 15px 50px;
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+    }
+
+    #nav #logo_url {
+        margin: auto;
+        margin-left: 0;
+    }
+
+    #logo {
+        height: 40px;
+        width: 40px;
+    }
+
+    #nav a {
+        color: #fcba03;
+        text-decoration: none;
+        margin: 16px;
+        transform: .5;
+    }
+
+    #nav a:hover {
+        color: #fff;
+    }
+</style>
